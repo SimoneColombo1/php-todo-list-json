13bb613e55027e8d2d81817cc2d7e27f79bb7286
SimoneColombo1 <simcolombo2@gmail.com>
2024-07-05 16:49:37 +0200

    modellato lo stile con css

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./CSS/style.css">
+    <title>todolist</title>
 </head>
 
 <body>
-
+    <header>
+        <h1>ToDoList</h1>
+    </header>
     <main id="app">
 
 
